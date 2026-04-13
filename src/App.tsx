ecae40cb4a18b87b57d105e37a2fcfcdc2184413
netoresende <raimundogomes826@gmail.com>
2026-04-13 19:01:23 -0300

    Recriando os componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css'
-
-function App() {
-  return (
-    <>
-      App,  Página principal do projeto
-    </>
-  )
+export function App (){
+    return (
+        <>
+            <h1>Olá mundo! ( do App )</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim laboriosam ab maiores vel? Voluptas at animi commodi a porro accusamus!
+            </p>
+        </>
+    )
 }
-export default App
