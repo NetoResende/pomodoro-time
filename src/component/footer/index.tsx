0b732d15bc0d09c799cbaa2c121c992fd2f28462
netoresende <raimundogomes826@gmail.com>
2026-04-21 16:32:47 -0300

    Criando aboutpomodoro e notFound

--- src/component/footer/index.tsx
+++ src/component/footer/index.tsx
@@ -4,7 +4,7 @@ export function Footer(){
 
     return (
         <footer className={styles.footer}>
-            <a>Entenda como funciona a técnica pomodoro</a>
+            <a href='/aboutPomodoro'>Entenda como funciona a técnica pomodoro</a>
             <a href="##">
                 Tarefas pomodoro &copy; {new Date().getFullYear()} - feito de coração!
             </a>
